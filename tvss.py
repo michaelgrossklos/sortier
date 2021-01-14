c61cd9877c5f8cc08837e70d9ec35310b1805509
Michael Grossklos <foofourtyone@gmail.com>
2021-01-14 22:19:43 +0100

    first time running without errors

--- tvss.py
+++ tvss.py
@@ -13,22 +13,25 @@ import click
 class TvShowSorter(object):
     logging.basicConfig(level = logging.WARNING, format = "%(msg)s")
     
-    TITLE = None
+    LANGUAGE = {
+        'de': 'Staffel',
+        'en': 'Season',
+        'es': 'Temporada',
+        'fr': 'Saison',
+        'it': 'Stagione',
+        'ru': 'Sezon',
+        }
     
-    def __init__(self, titles, origin_path, destination_path, extensions, persist_extensions, debug = False):
-        self.regex_season_episode = r"(s(\d{2})e\d{2})"
+    def __init__(self, language, origin_path, destination_path, extensions, debug = False):
         self.debug = debug
         self.conf = self.read_config_file()
-        self.LOG = self.start_logging()
+        self.regex_season_episode = self.conf['files']['regex']
         self.origin_path = origin_path or self.conf['paths']['origin_path']
         self.destination_path = destination_path or self.conf['paths']['destination_path']
         self.extensions = extensions or self.conf['files']['video_file_ext']
-        self.persist_extensions = persist_extensions
-        self.titles = titles
-        
-        click.echo(TvShowSorter.TITLE)
-        
-        click.echo(titles)
+        self.season = TvShowSorter.LANGUAGE[language]
+        self.LOG = self.start_logging()
+        self.titles = []
         
         try:
             os.chdir(self.conf['paths']['origin_path'])
@@ -58,39 +61,41 @@ class TvShowSorter(object):
         
         return title
     
-    def make_destination_path(self) -> None:
+    def make_destination_dir(self) -> None:
         if not os.path.exists(self.destination_path):
             os.mkdir(self.destination_path)
     
     def walk_show_files(self) -> None:
         for f in glob.glob(".", recursive = True):
-            if not re.search(self.titles, f, re.IGNORECASE):
-                continue
-            
-            for dir_path, dir_names, file_names in os.walk(f):
-                for filename in file_names:
-                    f_name, f_ext = os.path.splitext(filename)
-                    
-                    for i in self.extensions:
-                        if f_ext == i and f_name.find('sample') == -1:
-                            print(f_name)
-                            
-                            season_episode = re.search(self.regex_season_episode, f_name, re.IGNORECASE)
-                            print(season_episode)
-                            
-                            season_path = os.path.join(
-                                    self.destination_path, "Staffel " + season_episode.group(2)[1:])
-                            
-                            if not os.path.exists(season_path):
-                                os.mkdir(season_path)
-                            
-                            from_dir = os.path.join(self.origin_path, dir_path, f_name + f_ext)
-                            to_dir = os.path.join(
-                                    self.destination_path, season_path,
-                                    self.titles + " " + season_episode.group(1) + f_ext)
-                            
-                            if not os.path.isfile(to_dir):
-                                dest = shutil.copy2(from_dir, to_dir)
+            for title in self.titles:
+                if not re.search(self.make_show_title_regex(title), f, re.IGNORECASE):
+                    continue
+                
+                for dir_path, dir_names, file_names in os.walk(f):
+                    for filename in file_names:
+                        f_name, f_ext = os.path.splitext(filename)
+                        
+                        for i in self.extensions:
+                            if f_ext == i and f_name.find('sample') == -1:
+                                click.echo('Found: ', f_name)
+                                
+                                season_episode = re.search(self.regex_season_episode, f_name, re.IGNORECASE)
+                                click.echo('Episode: ', season_episode)
+                                
+                                season_path = os.path.join(
+                                        self.destination_path, self.season + " " + season_episode.group(2)[1:])
+                                
+                                if not os.path.exists(season_path):
+                                    os.mkdir(season_path)
+                                
+                                from_dir = os.path.join(self.origin_path, dir_path, f_name + f_ext)
+                                to_dir = os.path.join(season_path, title + " " + season_episode.group(1) + f_ext)
+                                
+                                if not os.path.isfile(to_dir):
+                                    shutil.copy2(from_dir, to_dir)
+    
+    def set_titles(self, tv_show_titles: list) -> None:
+        self.titles = tv_show_titles
 
 
 @click.group()
@@ -123,31 +128,28 @@ class TvShowSorter(object):
         help = "To extend the list of video file extensions"
         )
 @click.option(
-        "--persist-extensions",
-        envvar = "TVSS_PERSIST_EXTENSIONS",
+        "--language", "-l",
+        envvar = "TVS_LANGUAGE",
         required = False,
-        default = False,
-        type = click.BOOL,
-        help = "To persist the extensions into the config file. Only works in conjunction with --extensions"
+        default = "de",
+        type = click.STRING,
+        help = "Sets the language for season names"
         )
-@click.option(
-        '--titles', '-t',
-        envvar = "TVSS_TITLES",
-        multiple = True,
-        default = [],
-        required = True
-        )
-@click.pass_obj
-def cli(ctx, titles, origin_path, destination_path, extensions, persist_extensions, debug):
+@click.pass_context
+def cli(ctx, language, origin_path, destination_path, extensions, debug):
     """
     Sorting your ripped or downloaded tv-shows into folder of seasons.
     Renames all files like "Name Of Show s01e01.ext" for direct use in your
     media center like Plex. So it can find all meta data needed.
     """
-    ctx.obj = TvShowSorter(titles, origin_path, destination_path, extensions, persist_extensions, debug)
+    ctx.obj = TvShowSorter(language, origin_path, destination_path, extensions, debug)
 
 
 @cli.command()
+@click.argument('tv_show_titles', nargs = -1)
 @click.pass_context
-def fucking_command(ctx):
-    pass
+def titles(ctx, tv_show_titles):
+    """
+    The titles of the shows to be sorted. REQUIRED!
+    """
+    ctx.obj.set_titles(tv_show_titles)
